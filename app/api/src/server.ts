24b78ab0c1f23318d7bb45d1bf9201af390922c3
Nigel Rantor <wiggly@wiggly.org>
2025-02-17 01:29:29 +0000

    Fixed bosy-parser issue

--- app/api/src/server.ts
+++ app/api/src/server.ts
@@ -1,23 +1,22 @@
-import { plus } from '@wordle/domain/helper.js'
+import express from "express";
+import { createGameService } from "@wordle/domain/gameServiceImpl.js";
+import { createMemoryGameRepository } from "@wordle/domain/adapter/memoryGameRepository.js";
+import { createHttpGameService } from "@wordle/domain/adapter/httpGameService.js";
 
-import {isOk, Result, unwrapResult} from "@wordle/domain/result.js";
+const gameService = createGameService(createMemoryGameRepository());
 
-const answer = plus(23, 19);
+const app = express();
+app.use(express.urlencoded());
+app.use(express.json());
 
-function div(a: number, b: number): Result<number, string> {
-    if(b <= 0) {
-        return { err: "Division by zero" }
-    } else {
-        return { value: a / b }
-    }
-}
+//const routes = createHttpGameService(gameService, app);
+createHttpGameService(gameService, app);
 
-console.log(`Hello! ${answer}`);
+const port = process.env["PORT"] || 3000;
 
-let result = div(12, 0)
-
-if(isOk(result)) {
-    console.log(`${ unwrapResult(result) }`)
-} else {
-    console.log(`${result.err}`)
-}
+//const server = app.listen(port, () => {
+//    console.log(`Listening at http://localhost:${port}`);
+//});
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
